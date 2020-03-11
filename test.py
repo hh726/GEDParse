27c3bea7a87b9215d53f7c74e840105a4b5b6c45
Lachlan <lachlanmountjoy@gmail.com>
2020-03-11 19:39:42 -0400

    merging

--- test.py
+++ test.py
@@ -55,19 +55,19 @@ def cd5():
 #Lachlan
 
 def ca1501():
-	assert "ERROR: INDIVIDUAL: US07: 86: @I3@: More than 150 years old - Brith date 1771-03-7" in age_less_than_150
+	assert "ERROR: INDIVIDUAL: US07: 90: @I10@: More than 150 years old - Birth date 1500-01-1" in age_less_than_150
 
 def ca1502():
-	assert "ERROR: INDIVIDUAL: US07: 90: @I9@: More than 150 years old - Birth date 1270-08-4" in age_less_than_150
+	assert "ERROR: INDIVIDUAL: US07: 90: @I11@: More than 150 years old - Birth date 1500-02-2" in age_less_than_150
 
 def ca1503():
-	assert "ERROR: INDIVIDUAL: US07: 90: @I1@: More than 150 years old - Birth date 1859-02-1" in age_less_than_150
+	assert "ERROR: INDIVIDUAL: US07: 90: @I12@: More than 150 years old - Birth date 1500-03-3" in age_less_than_150
 
 def bbm1():
-	"ANOMALY: FAMILY: US08: 107: @F1@: Child @I1@ born 1859-02-1 before marriage on 1994-07-7" in birth_before_parents_marriage
+	assert "ANOMALY: FAMILY: US08: 107: @F1@: Child @I4@ born 1994-05-1 before marriage on 1994-07-7" in birth_before_parents_marriage
 
 def bbm2():
-	"ANOMALY: FAMILY: US08: 107: @F2@: Child @I3@ born 1771-03-7 before marriage on 1970-10-4" in birth_before_parents_marriage
+	assert "ANOMALY: FAMILY: US08: 107: @F3@: Child @I8@ born 1996-09-5 before marriage on 2021-09-7" in birth_before_parents_marriage
 
 
 
